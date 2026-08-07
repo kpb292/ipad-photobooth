b8d78d414d60a9c128bb6f800701bebe6a798a80
Komal Bachhawat <31674279+kpb292@users.noreply.github.com>
2026-08-07 16:17:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -356,4 +356,4 @@ $('#doneBtn').addEventListener('click', done);
 
 loadSettings();
 applyRatio(state.ratio);
-if ('serviceWorker' in navigator) navigator.serviceWorker.register('sw.js?v=8').catch(() => {});
+if ('serviceWorker' in navigator) navigator.serviceWorker.register('sw.js?v=13').catch(() => {});
